e0e102af15efb50ace30bca42c4e066932b89d9e
riley-1995 <rileydenn@gmail.com>
2025-05-01 21:19:05 -0700

    Add test cases and test on various implementations

--- Password.cpp
+++ Password.cpp
@@ -24,7 +24,122 @@ int Password::count_leading_characters(string phrase){
   receives a string and returns whether it has both at least one upper-case
   letter and at least one lower-case letter
 */
-bool Password::has_mixed_case(string pass){
+// bool Password::has_mixed_case(string pass){
+//   bool has_upper = false;
+//   bool has_lower = false;
+//   for (char c : pass) {
+//     if (isupper(c)) {
+//       has_upper = true;
+//     } else if (islower(c)) {
+//       has_lower = true;
+//     }
+//     if (has_upper && has_lower) {
+//       return true;
+//     }
+//   }  
+
+//   return false;
+// }
+
+// Implementation A - PASS
+// #include <cctype>
+// bool Password::has_mixed_case(string str)
+// {
+//   bool has_lower = false;
+//   bool has_upper = false;
+//   for (char ch : str)
+//   {
+//     if (std::islower(ch))
+//     {
+//       has_lower = true;
+//     }
+//     else if (std::isupper(ch))
+//     {
+//       has_upper = true;
+//     }
+//   }
+//   return has_lower && has_upper;
+// }
+
+// Implementation B
+// FAILED a0_mixed_case
+// FAILED 0a_mixed_case
+// FAILED middle_space_false_lower_mixed_case
+// bool Password::has_mixed_case(string str)
+// {
+//   int count_lower = 0;
+//   for (int i = 0; i < str.length(); i++)
+//   {
+//     if (str[i] >= 'a' && str[i] <= 'z')
+//     {
+//       count_lower++;
+//     }
+//   }
+//   return count_lower > 0 && count_lower < str.length();
+// }
+
+// Implementation C - PASS
+// bool Password::has_mixed_case(string str)
+// {
+//   bool lower = false;
+//   bool upper = false;
+//   for (int i = 0; i < str.length(); i++)
+//   {
+//     if (str[i] >= 'a' && str[i] <= 'z')
+//     {
+//       lower = true;
+//     }
+//     else if (str[i] >= 'A' && str[i] <= 'Z')
+//     {
+//       upper = true;
+//     }
+//   }
+//   return upper && lower;
+// }
+
+// Implementation D
+// [  FAILED  ] PasswordTest.a0_mixed_case
+// [  FAILED  ] PasswordTest.0a_mixed_case
+// [  FAILED  ] PasswordTest.middle_space_false_lower_mixed_case
+// #include <cctype>
+// bool Password::has_mixed_case(string str)
+// {
+//   bool is_lower = false;
+//   bool is_upper = false;
+//   for (char c : str)
+//   {
+//     if (std::islower(c))
+//     {
+//       is_lower = true;
+//     }
+//     else
+//     {
+//       is_upper = true;
+//     }
+//   }
+//   return is_lower && is_upper;
+// }
+
+// Implementation E
+// [  FAILED  ] PasswordTest.same_letter_lower_Upper_mixed_case
+// [  FAILED  ] PasswordTest.diff_letter_lower_Upper_mixed_case
+// [  FAILED  ] PasswordTest.trailing_mixed_case
+// [  FAILED  ] PasswordTest.0aA_mixed_case
+// [  FAILED  ] PasswordTest.special_char_true_mixed_case
+// [  FAILED  ] PasswordTest.special_char_true_mixed_case_4
+// [  FAILED  ] PasswordTest.special_char_true_mixed_case_5
+
+bool Password::has_mixed_case(string str)
+{
+  bool found = false;
+  for(char c : str){
+    if( !found && c >= 'A' && c <= 'Z' ){
+      found = true;
+    }
+    else if( found && c >= 'a' && c <= 'z'){
+      return true;
+    }
+  }
   return false;
 }
 
--- PasswordTest.cpp
+++ PasswordTest.cpp
@@ -14,14 +14,14 @@ class PasswordTest : public ::testing::Test
 		virtual void TearDown(){} //clean up after each test, (before destructor)
 };
 
-TEST(PasswordTest, single_letter_password)
+TEST(PasswordTest, single_letter_leading)
 {
 	Password my_password;
 	int actual = my_password.count_leading_characters("Z");
 	ASSERT_EQ(1, actual);
 }
 
-TEST(PasswordTest, empty_string)
+TEST(PasswordTest, empty_string_leading)
 {
 	Password my_password;
 	ASSERT_EQ(0, my_password.count_leading_characters(""));
@@ -110,59 +110,151 @@ TEST(PasswordTest, single_letter_Upper_mixed_case)
 	Password my_password;
 	ASSERT_FALSE(my_password.has_mixed_case("J"));
 }
-
 TEST(PasswordTest, single_letter_lower_mixed_case)
 {
 	Password my_password;
 	ASSERT_FALSE(my_password.has_mixed_case("j"));
 }
-
 TEST(PasswordTest, same_letter_Upper_lower_mixed_case)
 {
 	Password my_password;
 	ASSERT_TRUE(my_password.has_mixed_case("Jj"));
 }
-
 TEST(PasswordTest, same_letter_lower_Upper_mixed_case)
 {
 	Password my_password;
 	ASSERT_TRUE(my_password.has_mixed_case("jJ"));
 }
-
 TEST(PasswordTest, diff_letter_Upper_lower_mixed_case)
 {
 	Password my_password;
 	ASSERT_TRUE(my_password.has_mixed_case("Ja"));
 }
-
 TEST(PasswordTest, diff_letter_lower_Upper_mixed_case)
 {
 	Password my_password;
 	ASSERT_TRUE(my_password.has_mixed_case("aY"));
 }
-
 TEST(PasswordTest, leading_mixed_case)
 {
 	Password my_password;
 	ASSERT_TRUE(my_password.has_mixed_case("HEllo"));
 }
-
 TEST(PasswordTest, middle_mixed_case)
 {
 	Password my_password;
 	ASSERT_TRUE(my_password.has_mixed_case("aYYa"));
 }
-
 TEST(PasswordTest, trailing_mixed_case)
 {
 	Password my_password;
 	ASSERT_TRUE(my_password.has_mixed_case("aiYN"));
 }
-
-
-
-
-
+TEST(PasswordTest, a0_mixed_case)
+{
+	Password my_password;
+	ASSERT_FALSE(my_password.has_mixed_case("a0"));
+}
+TEST(PasswordTest, A0_mixed_case)
+{
+	Password my_password;
+	ASSERT_FALSE(my_password.has_mixed_case("A0"));
+}
+TEST(PasswordTest, 0a_mixed_case)
+{
+	Password my_password;
+	ASSERT_FALSE(my_password.has_mixed_case("0a"));
+}
+TEST(PasswordTest, 0A_mixed_case)
+{
+	Password my_password;
+	ASSERT_FALSE(my_password.has_mixed_case("0A"));
+}
+TEST(PasswordTest, 0aA_mixed_case)
+{
+	Password my_password;
+	ASSERT_TRUE(my_password.has_mixed_case("0aA"));
+}
+TEST(PasswordTest, 0Aa_mixed_case)
+{
+	Password my_password;
+	ASSERT_TRUE(my_password.has_mixed_case("0Aa"));
+}
+TEST(PasswordTest, 0AaA_mixed_case)
+{
+	Password my_password;
+	ASSERT_TRUE(my_password.has_mixed_case("0AaA"));
+}
+TEST(PasswordTest, AaA_mixed_case)
+{
+	Password my_password;
+	ASSERT_TRUE(my_password.has_mixed_case("AaA"));
+}
+TEST(PasswordTest, aAa_mixed_case)
+{
+	Password my_password;
+	ASSERT_TRUE(my_password.has_mixed_case("aAa"));
+}
+TEST(PasswordTest, middle_space_true_mixed_case)
+{
+	Password my_password;
+	ASSERT_TRUE(my_password.has_mixed_case("aAa a"));
+}
+TEST(PasswordTest, middle_space_false_lower_mixed_case)
+{
+	Password my_password;
+	ASSERT_FALSE(my_password.has_mixed_case("aba az"));
+}
+TEST(PasswordTest, middle_space_false_upper_mixed_case)
+{
+	Password my_password;
+	ASSERT_FALSE(my_password.has_mixed_case("ABA AZ"));
+}
+TEST(PasswordTest, empty_mixed_case)
+{
+	Password my_password;
+	ASSERT_FALSE(my_password.has_mixed_case(""));
+}
+TEST(PasswordTest, space_mixed_case)
+{
+	Password my_password;
+	ASSERT_FALSE(my_password.has_mixed_case(" "));
+}
+TEST(PasswordTest, space_mixed_case_2)
+{
+	Password my_password;
+	ASSERT_FALSE(my_password.has_mixed_case("  "));
+}
+TEST(PasswordTest, special_char_false_mixed_case)
+{
+	Password my_password;
+	ASSERT_FALSE(my_password.has_mixed_case("!@#$%^&*()"));
+}
+TEST(PasswordTest, special_char_true_mixed_case)
+{
+	Password my_password;
+	ASSERT_TRUE(my_password.has_mixed_case("!@#$%^&*()aA"));
+}
+TEST(PasswordTest, special_char_true_mixed_case_2)
+{
+	Password my_password;
+	ASSERT_TRUE(my_password.has_mixed_case("!@#$%^&*()Aa"));
+}
+TEST(PasswordTest, special_char_true_mixed_case_3)
+{
+	Password my_password;
+	ASSERT_TRUE(my_password.has_mixed_case("!@#$%^&*()0Aa"));
+}
+TEST(PasswordTest, special_char_true_mixed_case_4)
+{
+	Password my_password;
+	ASSERT_TRUE(my_password.has_mixed_case("!a#$%^&*()A4"));
+}
+TEST(PasswordTest, special_char_true_mixed_case_5)
+{
+	Password my_password;
+	ASSERT_TRUE(my_password.has_mixed_case("!a#$%^ &*()A4"));
+}
 
 
 
